4934f8c117c5a654cb09a2e033f3abee2eca5373
indious <sal@android-d767164a7004638d.localdomain>
2018-07-31 20:42:24 -0400

    addfile

---
+++ opensslwebcheck.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from OpenSSL import crypto
+
+def verify_certificate_chain(cert_path, trusted_certs):
+    # Download the certificate from the url and load the certificate
+    cert_file = open(cert_path, 'r')
+    cert_data = cert_file.read()
+    certificate = crypto.load_certificate(crypto.FILETYPE_PEM, cert_data)
+
+    #Create a certificate store and add your trusted certs
+    try:
+        store = crypto.X509Store()
+
+        # Assuming the certificates are in PEM format in a trusted_certs list
+        for _cert in trusted_certs:
+            cert_file = open(_cert, 'r')
+            cert_data = cert_file.read()
+            client_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, cert_data)
+            store.add_cert(client_certificate)
+
+        # Create a certificate context using the store and the downloaded certificate
+        store_ctx = crypto.X509StoreContext(store, certificate)
+
+        # Verify the certificate, returns None if it can validate the certificate
+        store_ctx.verify_certificate()
+
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+if __name__ == "__main__":
+    certs_dir = os.path.join(os.sep, "etc", "nginx", "certs")
+    cert_path = os.path.join(certs_dir, "client", "client.crt")
+    trusted_certs = [os.path.join(certs_dir, "ca", "ca.crt")]
+
+    if not verify_certificate_chain(cert_path, trusted_certs):
+        print("Invalid certificate!")
+        sys.exit(1)
+
+    print("Valid certificate!")
+    sys.exit(0)
